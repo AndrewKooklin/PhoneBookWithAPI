d6939f98dc74e6455e0536af6b7a7793ed4031c7
AndrewKooklin <bolt999@yandex.ru>
2024-03-01 14:07:58 +0300

    Add MyMultiValueConverter

---
+++ PhoneBookWPF/Data/DataModels/ModelBook.cs
@@ -0,0 +1,38 @@
+namespace PhoneBookWPF
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ModelBook : DbContext
+    {
+        public ModelBook()
+            : base("name=ModelBook")
+        {
+        }
+
+        public virtual DbSet<C__EFMigrationsHistory> C__EFMigrationsHistory { get; set; }
+        public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
+        public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUserRoles> AspNetUserRoles { get; set; }
+        public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
+        public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
+        public virtual DbSet<PhoneBookRecords> PhoneBookRecords { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspNetRoles>()
+                .HasMany(e => e.AspNetRoleClaims)
+                .WithRequired(e => e.AspNetRoles)
+                .HasForeignKey(e => e.RoleId);
+
+            modelBuilder.Entity<AspNetRoles>()
+                .HasMany(e => e.AspNetUserRoles)
+                .WithRequired(e => e.AspNetRoles)
+                .HasForeignKey(e => e.RoleId);
+        }
+    }
+}
